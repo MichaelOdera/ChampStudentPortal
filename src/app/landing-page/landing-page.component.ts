54a317a9beb6c085eee5272344a2359947fbd818
MichaelOdera <michogelira@gmail.com>
2020-07-05 04:24:28 +0300

    created a component that carries all the details of the landing page and named it landing-page.component.ts file

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { style, animate, transition, trigger } from '@angular/animations';
+import { ModalDirective } from 'angular-bootstrap-md';
+import { AuthenticationService } from '../authentication.service';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css'],
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1000ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('1000ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
+
+})
+export class LandingPageComponent implements OnInit {
+
+  @ViewChild(ModalDirective) modal: ModalDirective;
+
+  constructor(public authenticationService: AuthenticationService) { }
+
+  isShowDiv = false;
+
+  toggleDisplayDiv() {
+    this.isShowDiv = !this.isShowDiv;
+  }
+
+
+  ngOnInit(): void {
+  }
+
+}

e47b0d68d0f8e827da40ad05ee916da4a79ce3e3
MichaelOdera <michogelira@gmail.com>
2020-07-05 22:44:29 +0300

    included a function to enable the writing of the data that the user has entered into the database

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
+import { FirebaseUserModel } from './FirebaseUserModel';
 
 
 
@@ -13,8 +14,20 @@ import * as firebase from 'firebase/app';
 export class AuthenticationService {
   userData: any;
   user : firebase.User;
+  name: String;
+ 
+  firebaseuser: FirebaseUserModel;
+  defaultDatabase: firebase.database.Database;
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user !== null) ? true : false;
+  }
+
 
   constructor( public afAuth: AngularFireAuth, public afs: AngularFirestore, public router : Router, public ngZone : NgZone) {
+    this.firebaseuser = new FirebaseUserModel("", "");
+
     this.afAuth.authState.subscribe(user => {
       if (user) {
         this.userData = user;
@@ -27,23 +40,42 @@ export class AuthenticationService {
     })
    }
 
+ 
+
   SignUp(value) {
     return new Promise<any>((resolve, reject) => {
       firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
-      .then(res => {
-        this.router.navigate(['/dashboard']);
-        resolve(res);
+      .then(success => {
+        var newuser = {
+          name: value.name,
+          uid: success.user.uid,
+          email: success.user.email
+      }
+        success.user.updateProfile({
+          displayName: value.name
+        }).then(res => {
+          this.writeUserData(newuser)
+          this.router.navigate(['/dashboard']);
+          resolve(res);
+        })
+        
+        
       }, err => reject(err))
     })
+  } 
+
+  writeUserData(newuser: { name: string; uid: string; email: string; }) {
+    firebase.database().ref("/users"+ newuser.uid).set(newuser).catch( error =>
+      console.log("Error message " + error.message));
   }
-  
- 
- 
+
+
+
  
 
-  SignIn(email, password) {
-    return this.afAuth.signInWithEmailAndPassword(email, password)
-      .then((result) => {
+  SignIn(value) {
+    return this.afAuth.signInWithEmailAndPassword(value.email, value.password)
+      .then((_result) => {
         this.ngZone.run(() => {
           this.router.navigate(['dashboard']);
         })
@@ -61,6 +93,29 @@ export class AuthenticationService {
   }
 
 
-
-
+  getUserProfile() {      
+    let promise = new Promise((resolve, reject) => {
+        this.afAuth.currentUser
+            .then(res => {
+                if (res.providerData[0].providerId == 'password') {
+                    this.firebaseuser.name = res.displayName;
+                    this.firebaseuser.provider = res.providerData[0].providerId;
+                    return resolve(this.user);
+                }
+                else {
+                    this.firebaseuser.name = res.displayName;
+                    this.firebaseuser.provider = res.providerData[0].providerId;
+                    return resolve(this.user);
+                }
+            }, err => {
+                this.router.navigate(['login']);
+                return reject(err);
+            })
+    })
+    return promise;
 }
+}
+
+
+
+

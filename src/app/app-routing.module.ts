cf431e6c591da3827ba58e5b481e453bcb6011f1
MichaelOdera <michogelira@gmail.com>
2020-07-05 22:48:44 +0300

    added the path to the notfound component in the app-routing.module.ts file to ensure that the user of the application is aware when there is no internet connection or it is at a low connection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { StudentDashBoardComponent } from './student-dash-board/student-dash-board.component';
+import { AuthGuard } from './core/auth.guard';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
-  { path: 'landing', component: LandingPageComponent},
-  { path: 'registration', component: RegistrationComponent },
+  { path: 'landing', component: LandingPageComponent, canActivate: [AuthGuard]},
+  { path: 'registration', component: RegistrationComponent , canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: StudentDashBoardComponent}
+  { path: 'dashboard', component: StudentDashBoardComponent},
+  { path:'**', component : NotfoundComponent}
 ];
 
 @NgModule({

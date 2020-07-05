a5995dec6a21cf78c4e6ff8586579961cf80f8b9
MichaelOdera <michogelira@gmail.com>
2020-07-05 04:11:46 +0300

    added all the necessary credentails that are generated from the firebase console into the environment.ts file for save keeping

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBkTlM0ZXtutrJ3ds1H92DREo0-wcGoh_0",
+    authDomain: "dashboard-b1367.firebaseapp.com",
+    databaseURL: "https://dashboard-b1367.firebaseio.com",
+    projectId: "dashboard-b1367",
+    storageBucket: "dashboard-b1367.appspot.com",
+    messagingSenderId: "1069883354761",
+    appId: "1:1069883354761:web:db2495e94760fc5a726058",
+    measurementId: "G-VWXF9DXVY4"
+  }
 };
 
 /*

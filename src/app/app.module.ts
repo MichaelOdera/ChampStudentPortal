1baa06eaf99d840860d41e7279c19e35d6d45bf2
MichaelOdera <michogelira@gmail.com>
2020-07-05 04:14:29 +0300

    added the mdb bootstrap and the ngform and the ngprogress modules into the app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { StudentDashBoardComponent } from './student-dash-board/student-dash-board.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthenticationService } from './authentication.service';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    StudentDashBoardComponent,
+    LandingPageComponent,
+    NavbarComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule,
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthenticationService],
   bootstrap: [AppComponent]
+ 
 })
 export class AppModule { }

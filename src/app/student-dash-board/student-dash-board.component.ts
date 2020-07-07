9bc357dd56eb8d13545607e993d1d1e45a39d311
MichaelOdera <michogelira@gmail.com>
2020-07-07 18:19:37 +0300

    added the auth state listener for the user when he or she is logged in and also to enhance the function to fetch the username of the user and to display it on the dashboard once logged in

--- src/app/student-dash-board/student-dash-board.component.ts
+++ src/app/student-dash-board/student-dash-board.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirebaseUserModel } from '../FirebaseUserModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-dash-board',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentDashBoardComponent implements OnInit {
 
-  constructor() { }
+  userProfile: any;
+  name: string;
+  user: firebase.User;
+  displayProfile : FirebaseUserModel;
+
+  constructor( public authService: AuthenticationService, private router: Router, public afauth: AngularFireAuth) { 
+    this.userProfile = ""
+    //this.userDisplayName = this.router.getCurrentNavigation().extras.state.example; 
+    this.afauth.onAuthStateChanged(user => {
+        firebase.database().ref('users/'+user.uid).once("value", snap=>{
+          console.log(snap.val())
+          this.userProfile = snap.val().name
+          console.log("My User Name"+this.userProfile) 
+        })
+    })
+   }
 
   ngOnInit(): void {
+    this.afauth.currentUser.then(person =>{
+      this.name = person.email
+    })
+   
   }
 
 }

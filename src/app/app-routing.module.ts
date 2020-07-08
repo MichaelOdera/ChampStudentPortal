f478911432d8c75a2c7f4d5671e4bebce319f58c
MichaelOdera <michogelira@gmail.com>
2020-07-08 18:28:39 +0300

    added the authGuard definision to the path for the dashboard to ensure that only a user can access it once logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { StudentDashBoardComponent } from './student-dash-board/student-dash-board.component';
 import { AuthGuard } from './core/auth.guard';
@@ -10,10 +9,9 @@ import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
-  { path: 'landing', component: LandingPageComponent, canActivate: [AuthGuard]},
-  { path: 'registration', component: RegistrationComponent , canActivate: [AuthGuard]},
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: StudentDashBoardComponent},
+  { path: 'landing', component: LandingPageComponent, },
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'dashboard', component: StudentDashBoardComponent, canActivate: [AuthGuard]},
   { path:'**', component : NotfoundComponent}
 ];
 

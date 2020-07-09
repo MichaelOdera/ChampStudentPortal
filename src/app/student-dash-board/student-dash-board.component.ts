3d23cf93490d5d6b85d8beb880961543f3b1da14
MichaelOdera <michogelira@gmail.com>
2020-07-09 05:48:58 +0300

    added the main functionality that will ensure that data is read from the database and fetched by a promise and then made avaialble to be reflectted on the dash-board. the file that in which the function was written is student-dash-board.component.ts file

--- src/app/student-dash-board/student-dash-board.component.ts
+++ src/app/student-dash-board/student-dash-board.component.ts
@@ -4,6 +4,8 @@ import * as firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { FirebaseUserModel } from '../FirebaseUserModel';
 import { Router } from '@angular/router';
+import { GradeserviceService } from '../gradeservice.service';
+import { Result } from '../result.model';
 
 @Component({
   selector: 'app-student-dash-board',
@@ -12,26 +14,47 @@ import { Router } from '@angular/router';
 })
 export class StudentDashBoardComponent implements OnInit {
 
-  userProfile: any;
-  userEmail: string;
-  userData: any;
-  user: firebase.User;
-  userUid: any;
-  displayProfile : FirebaseUserModel;
-  key1: string;
-
+  firstResult: any;
+  finalResult: any;
   
+  constructor( public authService: AuthenticationService, private router: Router, public afauth: AngularFireAuth, public gradeService: GradeserviceService) { 
   
-
-  constructor( public authService: AuthenticationService, private router: Router, public afauth: AngularFireAuth) { 
-   
-
    }
 
+   sendResults(form){
+    this.gradeService.submitGrades(form.value)
+      // aler("Subjects Submitted Successfully");
+   }
 
+   logOut(){
+     this.authService.logout()
+   }
 
+   getId(id){
+     //console.log(id)
+     this.getGrades(id)
+   }
+
+  getGrades(id: any) {
+    return new Promise((resolve, reject) =>{
+      firebase.database().ref("users").child(id).child("grades").once('value', data => {
+          this.firstResult = data.val()
+          resolve(this.firstResult)
+      }, error => reject(error))
+    })
+  }
+
+  
   ngOnInit(): void {
-   
+
+    console.log(this.authService.getMeNow)    
+
+    console.log("The detail are here")
+
+    
+
+
+  
   }
 
   

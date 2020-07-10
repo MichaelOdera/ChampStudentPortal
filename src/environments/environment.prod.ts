9c5c0c265d9005439a7d8bc82bbd77b2068fbbf1
MichaelOdera <michogelira@gmail.com>
2020-07-10 11:47:34 +0300

    included the firebase configurations into the file environment.prod.ts for production purposes to enable the application to run on the production face

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkTlM0ZXtutrJ3ds1H92DREo0-wcGoh_0",
+    authDomain: "dashboard-b1367.firebaseapp.com",
+    databaseURL: "https://dashboard-b1367.firebaseio.com",
+    projectId: "dashboard-b1367",
+    storageBucket: "dashboard-b1367.appspot.com",
+    messagingSenderId: "1069883354761",
+    appId: "1:1069883354761:web:db2495e94760fc5a726058",
+    measurementId: "G-VWXF9DXVY4"
+  }
 };

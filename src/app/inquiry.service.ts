5927805f0d299cf8ea19a1c333b8c0fc5403e5ee
MichaelOdera <michogelira@gmail.com>
2020-07-10 11:33:54 +0300

    created a service inquiry.service.ts which is to allow the user to send an inquiry to the administration panel

---
+++ src/app/inquiry.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InquiryService {
+
+
+  constructor(public http: HttpClient, private router: Router) { }
+
+  sendEmail(email) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return new Promise((resolve, reject) => {
+       this.http.post('https://formspree.io/mrgawwny',
+      { name: email.name, _replyto: email.email, message: email.message },
+      { 'headers': headers }).toPromise().then(res => {
+        this.router.navigate(["inquirysuccess"])
+        resolve(res)
+      }, error => {
+      
+  
+        alert("Please Enter a Valid email to submit Inquiry")
+        reject(error)})
+    })
+   
+  }
+}

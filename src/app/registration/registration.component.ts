a14faf341e3b3b1ba56138e962a7955ce740d229
MichaelOdera <michogelira@gmail.com>
2020-07-10 12:05:49 +0300

    included the form varible as the placeholder for the values obtained from the form during signing up of a user for an application

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -23,15 +23,6 @@ export class RegistrationComponent implements OnInit {
 
   register(form){
     this.authService.SignUp(form.value)
-    .then(res => {
-      this.errorMessage = "";
-      alert("Successfully Created");
-      this.successMessage = "Your account has been created";
-    }, err => {
-      console.log(err);
-      this.errorMessage = err.message;
-      this.successMessage = "";
-    })
   }
 
 
